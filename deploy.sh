d48fe9d918b27c152f9603c77295e0588aca6989
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-06-07 08:01:28 -0500

    small fixes (remove scr_ from command list)

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ cd /Users/joshuaarnold/Documents/GitHub/Yean-Cat
 # Generate a list of all scripts
 find "YEAN CAT/scripts" -name "*.gml" -exec basename {} .gml \; > script_list.txt
 
-# Generate a list of all commands starting with scr_
-grep -r "function scr_" "YEAN CAT/scripts" | awk -F'function ' '{print $2}' | awk -F'(' '{print $1}' | sort -u > command_list.txt
+# Generate a list of all commands starting with scr_ and remove the scr_ prefix
+grep -r "function scr_" "YEAN CAT/scripts" | awk -F'function scr_' '{print $2}' | awk -F'(' '{print $1}' | sort -u > command_list.txt
 
 # Add and commit the updated lists
 git add script_list.txt command_list.txt
