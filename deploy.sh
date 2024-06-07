db0b6ed1bc2340495775277fb1b6fd46d1749c84
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-06-07 07:58:10 -0500

    more fixes

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cd /Users/joshuaarnold/Documents/GitHub/Yean-Cat
 find "YEAN CAT/scripts" -name "*.gml" -exec basename {} .gml \; > script_list.txt
 
 # Generate a list of all commands starting with scr_
-grep -r "function scr_" "YEAN CAT/scripts" | awk -F'/' '{print $NF}' | sed 's/^scr_//' | sed 's/.gml$//' > command_list.txt
+grep -r "function scr_" "YEAN CAT/scripts" | awk -F'function ' '{print $2}' | awk -F'(' '{print $1}' | sort -u > command_list.txt
 
 # Add and commit the updated lists
 git add script_list.txt command_list.txt
--- update_lists.sh
+++ update_lists.sh
@@ -4,8 +4,8 @@
 find "YEAN CAT/scripts" -name "*.gml" -exec basename {} .gml \; > script_list.txt
 
 # Generate a list of all commands starting with scr_
-grep -r "function scr_" "YEAN CAT/scripts" | awk -F'/' '{print $NF}' | sed 's/^scr_//' | sed 's/.gml$//' > command_list.txt
+grep -r "function scr_" "YEAN CAT/scripts" | awk -F'function ' '{print $2}' | awk -F'(' '{print $1}' | sort -u > command_list.txt
 
 # Commit and push the updated lists
 git add script_list.txt command_list.txt
-git commit -m "Update script and command lists"
+git commit -m "Update
