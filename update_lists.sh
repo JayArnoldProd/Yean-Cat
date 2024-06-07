9f754dce3fbc1246eed5810cfcc172ca460c2dfd
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-06-07 07:52:23 -0500

    more fixes

--- update_lists.sh
+++ update_lists.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Generate a list of all scripts
-find YEAN\ CAT/scripts -name "*.gml" > script_list.txt
+find "YEAN CAT/scripts" -name "*.gml" -exec basename {} .gml \; > script_list.txt
 
 # Generate a list of all commands starting with scr_
-grep -r "scr_" YEAN\ CAT/scripts/ | cut -d':' -f1 | grep ".gml" | sort -u | xargs -n1 basename | sed 's/scr_//' | sed 's/.gml//' > command_list.txt
+grep -rl "scr_" "YEAN CAT/scripts" | grep ".gml" | xargs -n1 basename | sed 's/^scr_//' | sed 's/.gml$//' > command_list.txt
 
 # Commit and push the updated lists
 git add script_list.txt command_list.txt
