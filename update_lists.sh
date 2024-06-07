8cdba0c8467365d5627804fd19c341eba31529f1
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-06-07 07:33:34 -0500

    Added files to set up smart gpt prompts

--- update_lists.sh
+++ update_lists.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Navigate to the project directory
-cd /Users/joshuaarnold/Documents/GitHub/Yean-Cat
-
 # Generate a list of all scripts
 find YEAN\ CAT/scripts -name "*.gml" > script_list.txt
 
 # Generate a list of all commands starting with scr_
 grep -r "scr_" YEAN\ CAT/scripts/ | cut -d':' -f1 | sort -u > command_list.txt
 
-# Add and commit the updated lists
+# Commit and push the updated lists
 git add script_list.txt command_list.txt
 git commit -m "Update script and command lists"
-
-# Push to Heroku
-git push heroku main
