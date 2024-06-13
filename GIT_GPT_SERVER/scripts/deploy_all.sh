57dbafe620501429581f6413a83890eb120e5a34
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-06-13 02:40:08 -0500

    Automated backup and deployment

--- GIT_GPT_SERVER/scripts/deploy_all.sh
+++ GIT_GPT_SERVER/scripts/deploy_all.sh
@@ -52,10 +52,10 @@ echo $FLASK_PID > /Users/joshuaarnold/Documents/GitHub/Yean-Cat/GIT_GPT_SERVER/f
 #    echo "Test script not found."
 #fi
 
-## Navigate to the project directory
-#cd /Users/joshuaarnold/Documents/GitHub/Yean-Cat
-#
-## Push to Heroku
-#git push heroku main
+# Navigate to the project directory
+cd /Users/joshuaarnold/Documents/GitHub/Yean-Cat
+
+# Push to Heroku
+git push heroku main
 
 echo "Deployment and run completed successfully!"
