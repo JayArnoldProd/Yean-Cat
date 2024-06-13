bf8bf4d9d4ce062007cc1f94103847a08e58c5b5
JayArnoldProd <jayarnoldproduces@gmail.com>
2024-06-13 15:22:22 -0500

    Updated backup scripts to exclude __pycache__ and __init__.py files

--- GIT_GPT_SERVER/scripts/deploy_all.sh
+++ GIT_GPT_SERVER/scripts/deploy_all.sh
@@ -7,25 +7,19 @@ source .env
 echo "Updating GitHub remote URL..."
 git remote set-url origin https://JayArnoldProd:${GITHUB_TOKEN}@github.com/JayArnoldProd/Yean-Cat.git
 
-# Step 3: Run force_push.sh with an optional commit message argument
-echo "Running force_push.sh..."
-./GIT_GPT_SERVER/scripts/force_push.sh "${1:-Catch up with local changes}"
-
-# Step 4: Run code_backup script with hierarchy generation
+# Step 3: Run code_backup script with hierarchy generation
 echo "Running backup script..."
 ./GIT_GPT_SERVER/scripts/code_backup.sh
 
-# Step 5: Identify and kill specific Flask processes
+# Step 4: Identify and kill specific Flask processes
 echo "Checking for and killing existing Flask servers on ports 5000..."
 ./GIT_GPT_SERVER/scripts/stop_server.sh
 
-# Step 6: Add and commit changes to GitHub (without push)
-echo "Adding and committing code_backups to GitHub..."
-git add .
-git commit -m "Automated backup and deployment"
-git push origin main --force
+# Step 5: Run force_push.sh with an optional commit message argument
+echo "Running force_push.sh..."
+./GIT_GPT_SERVER/scripts/force_push.sh "${1:-Catch up with local changes}"
 
-# Step 7: Check if the server is running and pull logs
+# Step 6: Check if the server is running and pull logs
 if curl -s --head http://localhost:5000 | grep "200 OK" > /dev/null; then
     echo "Pulling logs from the server..."
     curl -X POST http://localhost:5000/api/update_code/pull_logs -o Logs/server_logs.json
@@ -43,7 +37,7 @@ fi
 
 echo "Backup and deployment completed successfully!"
 
-# Step 8: Optionally run test API endpoints script
+# Step 7: Optionally run test API endpoints script
 echo "Running test API endpoints script..."
 if [ -f /Users/joshuaarnold/Documents/GitHub/Yean-Cat/GIT_GPT_SERVER/scripts/tests/test_api_endpoints.py ]; then
     python /Users/joshuaarnold/Documents/GitHub/Yean-Cat/GIT_GPT_SERVER/scripts/tests/test_api_endpoints.py
